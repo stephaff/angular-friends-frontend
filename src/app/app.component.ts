7c08dd72e2f480dcb74c65b066ea2c3c6f9ad98d
affali <affalistephane@gmail.com>
2023-06-25 09:52:54 +0000

    login OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, DoCheck } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
+
+  toolbar = false
 
   constructor(
     private router: Router
   ){}
+
+  ngDoCheck() {
+    const currentUrl = this.router.url
+    if(currentUrl == '/login' || currentUrl == '/signup'){
+      this.toolbar = false
+    }
+    else{
+      this.toolbar = true
+    }
+  }
 
   logout(){
     this.router.navigate(['login'])
